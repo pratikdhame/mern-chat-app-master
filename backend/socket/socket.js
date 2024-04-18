50761002ddbb7533879782f6a9b8e043b7ea0515
Pratik Dhame <96607747+pratiktechie@users.noreply.github.com>
2024-04-18 23:18:05 +0530

    update

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -2,12 +2,13 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
+
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
+		origin: ["https://mern-chat-app-master-97qp.vercel.app/login"],
 		methods: ["GET", "POST"],
 	},
 });
